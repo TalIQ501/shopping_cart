966649dab81afeb36ff2557b4f783c046abfe201
TalIq501 <talhaiqbal501@gmail.com>
2025-04-03 01:17:19 +0500

    Fixed Product Page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -3,18 +3,21 @@ import { useParams } from "react-router-dom";
 import styles from "./ProductPage.module.css";
 import { ProductsContext } from "../../contexts/ProductsContext.jsx";
 import { useContext } from "react";
+import { BtnAddToCart } from "../../components/BtnAddToCart/BtnAddToCart.jsx"
 
 export const ProductPage = () => {
-  const { productID } = useParams();
+  const { id } = useParams();
 
-  const { products } = useContext(ProductsContext);
+  const { products, loading } = useContext(ProductsContext);
 
-  const product = products.find((p) => p.id === productID);
+  const product = products.find((p) => p.id === Number(id));
+
+  if (loading) return <h2>Loading...</h2>
 
   if (!product) return <h2>Product not found</h2>;
 
   return (
-    <div className={styles.product}>
+    <section className={styles.product}>
       <div className={styles.imgContainer}>
         <img src={product.image} alt="" />
       </div>
@@ -27,8 +30,8 @@ export const ProductPage = () => {
         <div className={styles.detail}>${product.price}</div>
         <div className={styles.detailLabel}>Rating</div>
         <div className={styles.detail}>{product.rating.rate}⭐</div>
-        <div>{}</div>
       </div>
-    </div>
+      <BtnAddToCart id={id} />
+    </section>
   );
 };
